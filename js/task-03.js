31132a897bffb42352e4856b7296d33bbe241c57
stas-yushkov <stas.yushkov@gmail.com>
2021-06-03 01:23:01 +0300

    task 03 done

--- js/task-03.js
+++ js/task-03.js
@@ -39,4 +39,55 @@ const images = [
 ];
 
 const galleryRef = document.querySelector('ul#gallery');
-console.log(galleryRef);
+// console.log(galleryRef);
+// galleryRef.innerHTML = '<li><img />112</li>';
+// console.log(galleryRef.innerHTML);
+
+const createGallery = (imagesArr, ulInHTML) => {
+  ulInHTML.innerHTML = imagesArr
+    .map(image => `<li><img src='${image.url}' alt='${image.alt}' /></li>`)
+    .join('');
+};
+
+createGallery(images, galleryRef);
+
+const flexGridGap = 20;
+const bodyRef = document.querySelector('body');
+bodyRef.style.width = '1170px';
+bodyRef.style.paddingLeft = `${flexGridGap}px`;
+bodyRef.style.paddingRight = `${flexGridGap}px`;
+bodyRef.style.backgroundColor = '#c7c9b1';
+bodyRef.style.marginLeft = 'auto';
+bodyRef.style.marginRight = 'auto';
+bodyRef.style.boxSizing = 'border-box';
+bodyRef.style.display = ' block';
+
+galleryRef.style.display = 'flex';
+galleryRef.style.flexWrap = 'wrap';
+galleryRef.style.padding = 0;
+galleryRef.style.margin = 0;
+galleryRef.style.marginTop = ` ${-flexGridGap}px`;
+galleryRef.style.marginLeft = ` ${-flexGridGap}px`;
+galleryRef.style.listStyle = 'none';
+galleryRef.style.boxSizing = 'border-box';
+
+[...galleryRef.children].map(li => {
+  li.style.flexBasis = `calc( 100% / 3 -  ${flexGridGap}px)`;
+  li.style.marginTop = ` ${flexGridGap}px`;
+  li.style.marginLeft = `${flexGridGap}px`;
+  li.style.borderRadius = '10px';
+  li.style.backgroundColor = '#b4e39f';
+  li.style.boxSizing = 'border-box';
+  li.style.maxHeight = '260px';
+  li.style.display = 'block';
+  li.style.overflow = 'hidden';
+});
+
+[...galleryRef.children].map(li => {
+  // li.firstChild.setAttribute('width', 270);
+  li.firstChild.style.display = 'block';
+  li.firstChild.style.maxWidth = '100%';
+  li.firstChild.style.height = '100%';
+  li.firstChild.style.objectFit = 'cover';
+  li.firstChild.style.boxSizing = 'border-box';
+});
