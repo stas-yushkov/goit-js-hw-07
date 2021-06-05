138d51f850ed2f84cb08ad7a771a7967633f34d1
stas-yushkov <stas.yushkov@gmail.com>
2021-06-06 00:52:29 +0300

    06

--- js/task-06.js
+++ js/task-06.js
@@ -22,3 +22,47 @@
 // #validation-input.invalid {
 //   border-color: #f44336;
 // }
+
+const inputRef = document.querySelector('input#validation-input');
+
+const handleBlur = e => {
+  if (
+    Number(e.currentTarget.dataset.length) ===
+    //=== e.currentTarget.value.trim().length
+    e.currentTarget.value.length
+  ) {
+    // console.log(
+    //   `Введено подходящее количество символов - ${e.currentTarget.value.length}`,
+    // );
+    // console.log(`Ввели: '${e.currentTarget.value}'`);
+    // console.log(
+    //   `Строка после удаления пробелов начале и в конце: '${e.currentTarget.value.trim()}'`,
+    // );
+    if (e.currentTarget.classList.contains('invalid')) {
+      e.currentTarget.classList.remove('invalid');
+    }
+    e.currentTarget.classList.add('valid');
+  } else if (e.currentTarget.value.length !== 0) {
+    // console.log(
+    //   `Введено неправильное количество символов - ${e.currentTarget.value.length}. Пробелы в начале и в конце строки удаляются`,
+    // );
+    // console.log(`Ввели: '${e.currentTarget.value}'`);
+    // console.log(
+    //   `Строка после удаления пробелов начале и в конце: '${e.currentTarget.value.trim()}'`,
+    // );
+    if (e.currentTarget.classList.contains('valid')) {
+      e.currentTarget.classList.remove('valid');
+    }
+    e.currentTarget.classList.add('invalid');
+  } else {
+    // console.log('Ничего не введено');
+    if (e.currentTarget.classList.contains('valid')) {
+      e.currentTarget.classList.remove('valid');
+    }
+    if (e.currentTarget.classList.contains('invalid')) {
+      e.currentTarget.classList.remove('invalid');
+    }
+  }
+};
+
+inputRef.addEventListener('blur', handleBlur);
